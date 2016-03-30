7fa5a2dd3a7d71634b9d1d60969191a437ec11f0
Gashenko Anna <janngash@gmail.com>
2016-03-30 09:52:21 +0300

    update

--- test_job.php
+++ test_job.php
@@ -5,8 +5,11 @@ class UnicumCode {
     protected $cod;
 
 	public function __construct() {
-		$this->cod = uniqid();
+		// Эта функция возвращает уникальный идентификатор, основываясь на значениях текущего времени в микросекундах
+		//$this->cod = uniqid();
+		// обрабатываем md5
 		$this->cod = md5(uniqid(rand(),1));
+		// возращает последние 10 символов
 		$this->cod = substr($this->cod,-10);
 	}
 }
@@ -15,9 +18,13 @@ class UnicumCode {
 class ChangeToNumbers extends UnicumCode {
 
 	public function createNumber() {
+		// находим все латинские буквы
 		preg_match_all('#[a-z]#ui',$this->cod,$matches);
+		// Создает массив, содержащий диапазон элементов от 0 до 9
 		$replacements = range(0,9);
+		// shuffle — Перемешивает массив
 		shuffle($replacements);
+		// заменяем найденные символы на цифры
 		$result = str_replace($matches[0], $replacements, $this->cod);
 		return $result;
 	}
@@ -27,9 +34,13 @@ class ChangeToNumbers extends UnicumCode {
 class ChangeToAbc extends UnicumCode {
 
 	public function createLetters() {
+		// находим все цифры
 		preg_match_all('#\d#ui',$this->cod,$matches);
+		// Создает массив, содержащий диапазон элементов от a до z
 		$replacements = range('a','z');
+		// shuffle — Перемешивает массив
 		shuffle($replacements);
+		// заменяем найденные цифры на буквы латинского алфавита
 		$result = str_replace($matches[0], $replacements, $this->cod);
 		return $result;
 	}
@@ -38,9 +49,13 @@ class ChangeToAbc extends UnicumCode {
 class ChangeLatinToCyrillic extends UnicumCode {
 
 	public function createCyrillic() {
+		// находим все латинские буквы
 		preg_match_all('#[a-z]#ui',$this->cod,$matches);
+		// Создает массив, содержащий буквы кириллического алфавита
 		$replacements = array("a","б","в","г","д","е","ж","з","и","к","л","м","н","о","п","р","с","т","у","ф","х","ц","ч","ш","щ","э","ю","я");
+		// shuffle — Перемешивает массив
 		shuffle($replacements);
+		// заменяем найденные буквы латинского алфавита на буквы кириллического алфавита
 		$result = str_replace($matches[0], $replacements, $this->cod);
 		return $result;
 	}
