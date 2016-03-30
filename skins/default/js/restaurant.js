9615f46e5a191f637fa21210989e161c8065d3ed
Anna Gashenlo <janngash@gmail.com>
2016-03-30 11:24:27 +0300

    Update restaurant.js

--- skins/default/js/restaurant.js
+++ skins/default/js/restaurant.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
 
 				for (var i in response.newOrder) {
 					$('.lastOrders').prepend('<tr><td><a href="/restaurant/dishes2order?id='+response.newOrder[i].id+'">'+response.newOrder[i].id+'</a></td><td>'+response.newOrder[i].weiter_name+'</td><td>'+response.newOrder[i].date+'</td><td>'+response.newOrder[i].summ_order+'</td></tr>');
-	            	$('.lastOrders tr:last').remove();
+	            			$('.lastOrders tr:last').remove();
 				}
 
 				for (var i in response.newSumm) {
@@ -24,8 +24,8 @@ $(document).ready(function() {
 				}
 
 				for (var i in response.popularDishes) {
-	            	$('.popular5dishes').prepend('<tr><td>'+response.popularDishes[i].id+'</td><td>'+response.popularDishes[i].title_dish+'</td><td>'+response.popularDishes[i].price+'</td></tr>');
-	            	$('.popular5dishes tr:last').remove();
+	            			$('.popular5dishes').prepend('<tr><td>'+response.popularDishes[i].id+'</td><td>'+response.popularDishes[i].title_dish+'</td><td>'+response.popularDishes[i].price+'</td></tr>');
+	            			$('.popular5dishes tr:last').remove();
 				}
 			}													
 		});
